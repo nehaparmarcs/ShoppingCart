a421600586c75a8bb934d7ad6650242518516623
nehaparmarcs <np6.023@gmail.com>
2016-04-14 02:31:44 -0700

    CouchDB connectivity.

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Github account list.
 
 Long Nguyen: longnguyen0708.
+Neha Parmar: nehaparmarcs.
---
+++ src/main/java/com/sjsu/service/catalogue/ICatalogService.java
@@ -0,0 +1,32 @@
+package com.sjsu.service.catalogue;
+
+import com.sjsu.model.Item;
+import com.sjsu.model.UserRequest;
+import com.sjsu.model.UserResponse;
+
+/**
+ * Allows shoppingManager to browse Item on the basis of searchText entered.
+ * 
+ * @author neha
+ *
+ */
+public interface ICatalogService {
+
+	/**
+	 * Called when user hits the site and chooses to browse for all items.
+	 * @param searchText performs the search on the basis of searchText if entered by the user
+	 * else returns from popular items default for the page.
+	 * @return itemList to display on the page
+	 */
+	public Item[] browse(String searchText);
+	
+	/**
+	 * This method returns the complete description of an item when it is selected by the User.
+	 * @param itemID performs the detailed search on the basis of itemID
+	 * @return Items complete description
+	 */
+	public Item itemDetails(String itemID);
+	
+	
+	
+}
