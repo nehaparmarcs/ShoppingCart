1685586ccb38f60e5d0d82b3a0cfcc62add3c58a
navdeepdahiya <navdeepdahiya@10.0.0.17>
2016-04-15 21:09:45 -0700

    Login service interface

---
+++ src/main/java/com/sjsu/service/login/ILoginService.java
@@ -0,0 +1,23 @@
+package com.sjsu.service.login;
+
+/**
+ * Service for login operations
+ * @author navdeepdahiya
+ *
+ */
+
+public interface ILoginService {
+
+	/**
+	 * Authenticates a user who logs in with required details.
+	 * @return true if the user is authentic false otherwise
+	 */
+	public boolean authenticate();
+	
+	/**
+	 * Creates a user.
+	 * @return returns true if user creation was successful
+	 */
+	public boolean createUser();
+	
+}
