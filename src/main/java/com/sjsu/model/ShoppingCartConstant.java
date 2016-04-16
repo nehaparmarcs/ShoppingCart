38d476b7e36c2ffc721c44adf791ee72b0e24014
nehaparmarcs <np6.023@gmail.com>
2016-04-15 23:56:40 -0700

    Added constants for Controller

--- src/main/java/com/sjsu/model/ShoppingCartConstant.java
+++ src/main/java/com/sjsu/model/ShoppingCartConstant.java
@@ -10,5 +10,8 @@ public interface ShoppingCartConstant {
 	public static final int IDENTIFIED_USER_TYPE = 0;
 	public static final int ANONYMOUS_USER_TYPE = 1;
 	
+	public static final String SUCCESS = "success";
+	public static final String FAILURE = "Failure";
+	public static final String ERROR_PAGE = "/jsp/errorPage.jsp";
 	
 }
